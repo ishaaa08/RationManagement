7f94146f1e88ca4342afa6cc4b1a09c18db6f79a
ishanigam <ishanigam0802@gmail.com>
2025-01-27 23:10:23 +0530

    firstcommit

--- src/main/java/com/example/RationManagementSystem/Controller/Controller.java
+++ src/main/java/com/example/RationManagementSystem/Controller/Controller.java
@@ -3,6 +3,7 @@ import com.example.RationManagementSystem.Entity.RationEntity;
 import com.example.RationManagementSystem.Repository.SaveDb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.yaml.snakeyaml.events.Event;
 
 import java.util.List;
 
@@ -17,15 +18,29 @@ import java.util.List;
         @Autowired
         private SaveDb repository;
 
-        @GetMapping
+        @GetMapping("/all/entities")
         public List<RationEntity> getAllEntities() {
+
             return repository.findAll();
         }
 
-        @PostMapping
+        @PostMapping("save/entity")
         public RationEntity createEntity(@RequestBody RationEntity entity) {
             return repository.save(entity);
         }
+    @PostMapping("/checkByID")
+    public boolean existsById(Long id) {
+        return repository.existsById(id);
+    }
+
+//        @PostMapping("/deleteByID")
+//    public String deleteEntityById(@RequestParam String id) {
+//            return repository.deleteById(id);
+//        }
+    @PostMapping("/findOne")
+    public RationEntity findOne(Long id) {
+            return repository.findOne(id);
+    }
     }
 
 
--- src/main/java/com/example/RationManagementSystem/Repository/SaveDb.java
+++ src/main/java/com/example/RationManagementSystem/Repository/SaveDb.java
@@ -6,7 +6,9 @@ package com.example.RationManagementSystem.Repository;
 
 import com.example.RationManagementSystem.Entity.RationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SaveDb extends JpaRepository<RationEntity, Long> {
     // Add custom query methods if needed
 }
